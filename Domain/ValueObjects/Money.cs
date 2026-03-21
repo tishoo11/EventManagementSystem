d9526b32e268d6593b80ff9a7a0d98ca558e1a51
Kebabche11 <tishoyanev08@gmail.com>
2026-03-21 10:50:12 +0200

    Created TicketCategory.cs in enums and Money.cs in ValueObjects

---
+++ Domain/ValueObjects/Money.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Domain.ValueObjects
+{
+    public class Money
+    {
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = "BGN";
+    }
+}
