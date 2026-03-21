2f850080ce913cc197f72636eb81685766652bf8
Kebabche11 <tishoyanev08@gmail.com>
2026-03-21 10:51:05 +0200

    added class Event to entities

---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string LocationName { get; set; } 
+        public int Capacity { get; set; }
+        public string EventType { get; set; }
+    }
+}
