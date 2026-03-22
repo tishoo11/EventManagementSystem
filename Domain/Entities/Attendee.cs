7c574f68476e7c41aaef76eb9cb095e8b3bcb8d9
eange <eangelova543@gmail.com>
2026-03-22 15:52:37 +0200

    class Attendee

---
+++ Domain/Entities/Attendee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Domain.Entities
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
