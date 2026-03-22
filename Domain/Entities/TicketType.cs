8c2e93475522e4f3184c21640af36c996fed467e
eange <eangelova543@gmail.com>
2026-03-22 15:55:21 +0200

    class TicketType

---
+++ Domain/Entities/TicketType.cs
@@ -0,0 +1,17 @@
+﻿using Event_Management_System.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Domain.Entities
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Money Price { get; set; }
+    }
+
+}
