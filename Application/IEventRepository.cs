1ec38075948d8922d920e24ca4c8291263923bb6
Kebabche11 <tishoyanev08@gmail.com>
2026-04-25 09:54:07 +0300

    Added IEventRepository

---
+++ Application/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using Event_Management_System.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Application
+{
+    public interface IEventRepository
+    {
+        List<Event> GetEvents(); void SaveEvents(List<Event> list);
+        List<Ticket> GetTickets(); void SaveTickets(List<Ticket> list);
+        List<string> GetLocations(); void SaveLocations(List<string> list);
+        List<Organizer> GetOrganizers(); void SaveOrganizers(List<Organizer> list);
+        List<TicketType> GetTicketTypes(); void SaveTicketTypes(List<TicketType> list);
+    }
+}
