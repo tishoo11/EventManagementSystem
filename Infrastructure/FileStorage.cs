1c3d305aa2ea3a3efe97541ae4d8979f6090838d
eange <eangelova543@gmail.com>
2026-05-03 12:44:52 +0300

    zapisvane v json

--- Infrastructure/FileStorage.cs
+++ Infrastructure/FileStorage.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using System.IO;
+using System.Text.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Event_Management_System.Infrastructure
+namespace EventManagement.Infrastructure
 {
-    internal class FileStorage
+    public static class FileStorage
     {
+        public static List<T> Load<T>(string file)
+        {
+            if (!File.Exists(file)) return new List<T>();
+            return JsonSerializer.Deserialize<List<T>>(File.ReadAllText(file));
+        }
+        public static void Save<T>(string file, List<T> data)
+        {
+            File.WriteAllText(file, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true }));
+        }
     }
 }
